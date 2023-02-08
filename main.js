e1bab807cef9b085b5be13042524fd801f9c3e6a
Zinah-AZ <105180491+Zinah-Zwayen@users.noreply.github.com>
2023-02-08 14:21:54 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ if(emailInput.value.match(validRegex)){
     emailForm.classList.add("valid");
     emailForm.classList.remove("invalid");
     emailError.innerHTML = "Your email address is valid";
-    emailError.style.cssText = "color:green; font-size:12px; display:flex; padding-left: 17px"
+    emailError.style.cssText = "color:green; font-size:12px; display:flex; padding-left: 17px;"
+    emailInput.style.borderColor = "green";
 }
 else{
     emailForm.classList.remove("valid");
@@ -20,16 +21,9 @@ else{
     emailInput.style.border = "1px solid";
     emailInput.style.borderColor = "red";
 }
-if(emailInput.value == ""){
-    emailForm.classList.remove("valid");
-    emailForm.classList.remove("invalid");
-    emailError.innerHTML = "";
-    emailError.style.cssText = "color:red; font-size:12px; display:flex; padding-left: 17px"
-    emailInput.style.borderColor = "red";
-}
 }
 
-submitButton.addEventListener('click', e => {
+submitButton.addEventListener('submit', e => {
     e.preventDefault();
     emailValidation();
 });
